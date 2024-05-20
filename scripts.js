bf6a2cf4d30786eaa9b7309a925b5b3b95abe704
cho4u4o <cho4u4o@gmail.com>
2024-05-21 00:33:02 +0900

    feat : UI update(bg color, header etc)

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("scrollButton").addEventListener("click", function () {
+  window.scrollTo({
+    top: 1000, // 원하는 스크롤 위치로 조정
+    behavior: "smooth", // 부드럽게 스크롤되도록 설정
+  });
+});
